e6654a40eb3a5cb692b0312cc840bf5a51857421
Park Seung Ho <dreamhigh924@gmail.com>
2024-10-30 16:58:48 +0900

    Practice Section 3

---
+++ Section3/src/main/java/udemy/springsecurityapp/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package udemy.springsecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
