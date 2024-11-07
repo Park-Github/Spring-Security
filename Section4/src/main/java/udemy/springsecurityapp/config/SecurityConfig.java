08030c505cbbaa63b96ba4b921ad533bb23ef1bc
Park Seung Ho <dreamhigh924@gmail.com>
2024-11-08 00:00:00 +0900

    Practice Section 4

--- Section4/src/main/java/udemy/springsecurityapp/config/SecurityConfig.java
+++ Section4/src/main/java/udemy/springsecurityapp/config/SecurityConfig.java
@@ -16,9 +16,10 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
+        http    .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) ->
                 requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                        .requestMatchers("/notices", "contact" ,"/error").permitAll());
+                        .requestMatchers("/notices", "contact" ,"/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
